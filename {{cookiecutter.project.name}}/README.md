a65adfcd608adbaef56600018ec20abbae3ea712
Damian Silbergleith <dsilbergleith+github@gmail.com>
2025-01-05 08:01:10 -0700

    initial commit

---
+++ {{cookiecutter.project.name}}/README.md
@@ -0,0 +1,3 @@
+# {{cookiecutter.project_name}}
+
+A basic C project created with a template.
---
+++ {{cookiecutter.project.name}}/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello, {{cookiecutter.project_name}}!\n");
+  return 0;
+}
